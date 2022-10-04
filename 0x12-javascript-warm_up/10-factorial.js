a3eaeebff12006c356b22e855e21e748e18a2c6f
Ahortu90 <ahortuderrick4@gmail.com>
2022-10-04 20:22:33 +0000

    update

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,17 +1,12 @@
 #!/usr/bin/node
-const process = require('process');
-
-function factorialize (num) {
-  if (isNaN(num)) {
-    return 1;
+function factorial (n) {
+    if (n < 0) {
+      return (-1);
+    }
+    if (n === 0 || isNaN(n)) {
+      return (1);
+    }
+    return (n * factorial(n - 1));
   }
-  if (num < 0) {
-    return -1;
-  } else if (num === 0) {
-    return 1;
-  } else {
-    return (num * factorialize(num - 1));
-  }
-}
-
-console.log(factorialize(parseInt(process.argv[2])));
+  
+  console.log(factorial(Number(process.argv[2])));
