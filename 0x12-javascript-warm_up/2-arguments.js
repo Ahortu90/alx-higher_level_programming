f9165daffe8866c1e60221997e73783a58ee1bac
Ahortu90 <ahortuderrick4@gmail.com>
2022-10-04 14:21:11 +0000

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 const count = process.argv.length;
-console.log(count === 1 ? 'No argument' : count === 2 ? 'Argument found' : 'Arguments found');
+console.log(count === 1 ? 'No argument');
+console.log(count === 2 ? 'Argument found');
+console.log('Arguments found');
